24c26cbc32ea88fd6523a253a1620e082cb938b8
farisubuntu <farisubuntu@gmail.com>
2020-08-30 02:48:47 +0300

    adding Javascript Arrays.jpg,.mm,.tree.html files

---
+++ Javascript Arrays.mm
@@ -0,0 +1,596 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Javascript Arrays" FOLDED="false" ID="ID_1027807873" CREATED="1598603789283" MODIFIED="1598743140776" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="0.939">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" BACKGROUND_COLOR="#ff9966">
+<font SIZE="9"/>
+<edge STYLE="sharp_bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Array-Like Objects" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" POSITION="right" ID="ID_295997094" CREATED="1598603803918" MODIFIED="1598743140775" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="-117.99999606609357 pt" VSHIFT_QUANTITY="-77.24999769777068 pt">
+<edge COLOR="#ff0000"/>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1793380485" CREATED="1598603830822" MODIFIED="1598737806656" HGAP_QUANTITY="12.500000044703482 pt" VSHIFT_QUANTITY="-17.99999946355821 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0000">what is array-like objects</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_2050344" CREATED="1598730728731" MODIFIED="1598740933874">
+<icon BUILTIN="idea"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Object representations of Arrays with a length property.</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_591291573" CREATED="1598730777011" MODIFIED="1598737813332"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0000">Example of user <b><i>array-like objects</i></b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1601395023" CREATED="1598730895870" MODIFIED="1598739608839" HGAP_QUANTITY="16.249999932944775 pt" VSHIFT_QUANTITY="-14.999999552965178 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>&#160;The <i><u>arguments</u></i>&#160;object passed to functions</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1398819685" CREATED="1598730876524" MODIFIED="1598737817764" HGAP_QUANTITY="91.99999767541892 pt" VSHIFT_QUANTITY="19.49999941885473 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0000">Common Examples</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1880944845" CREATED="1598730928651" MODIFIED="1598737946173"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#0000c0"><b><i>HTMLCollection or NodeList - returned from document.getElementsByTagName OR document.querySelectorAll()</i></b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1699981804" CREATED="1598603841902" MODIFIED="1598737832255" HGAP_QUANTITY="92.74999765306718 pt" VSHIFT_QUANTITY="74.24999778717763 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0000">The key difference between <b>Arrays </b>and <b>Array-Like</b>&#160;objects</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1256378776" CREATED="1598731621480" MODIFIED="1598737946175"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#0000c0">Array-like </font></b><font color="#0000c0">objects inherit from <b>Object.prototype</b>&#160;instead of <b>Array.prototype</b>. <i>This means that <b>Array-like</b>&#160;Objects can't access common Array prototype methods like <b>forEach(), push(), map(), filter()</b>, and <b>slice()</b></i></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1791810269" CREATED="1598604565786" MODIFIED="1598740691547" HGAP_QUANTITY="73.99999821186071 pt" VSHIFT_QUANTITY="33.74999899417165 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff0000">Convert Array-Like Objects</font>
+    </p>
+    <p>
+      <font color="#ff0000">into Array objects.</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="styles.topic" ID="ID_972752338" CREATED="1598721494691" MODIFIED="1598738440597" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="18.749999441206477 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      version &lt;= <b><i>ES5</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_355840837" CREATED="1598736472083" MODIFIED="1598737981883" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="-41.24999877065424 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Use Array.prototype.slice</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1375212673" CREATED="1598736637506" MODIFIED="1598739702584"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>var arrayLike = { </b>
+    </p>
+    <p>
+      <b>0: 'Value 0', </b>
+    </p>
+    <p>
+      <b>1: 'Value 1', </b>
+    </p>
+    <p>
+      <b>length: 2 </b>
+    </p>
+    <p>
+      <b>}; </b>
+    </p>
+    <p>
+      <b><font color="#008000">var realArray = Array.prototype.slice.call(arrayLike); </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">realArray = [].slice.call(arrayLike); // Shorter version</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_926829183" CREATED="1598737138864" MODIFIED="1598737981885"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Use <font color="#ff0000">Function.prototype.call</font>&#160;to call Array.prototype methods on Array-like objects directly, without converting them</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1952369828" CREATED="1598737744688" MODIFIED="1598739675092"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#008000">Array.prototype.forEach.call(domList, function() { </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">// Do stuff </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">});</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_715681291" CREATED="1598739735091" MODIFIED="1598739761276"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>use [].method.bind( arrayLikeObject ) to borrow array methods and glom them on to your object</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_503283211" CREATED="1598739765905" MODIFIED="1598739900083"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      // Version &#8805; 5.1
+    </p>
+    <p>
+      var arrayLike = {
+    </p>
+    <p>
+      0: 'Value 0',
+    </p>
+    <p>
+      1: 'Value 1',
+    </p>
+    <p>
+      length: 2
+    </p>
+    <p>
+      };
+    </p>
+    <p>
+      <font color="#008000">[].forEach.bind(arrayLike)(function(val){ </font>
+    </p>
+    <p>
+      <font color="#008000">// Do stuff with val </font>
+    </p>
+    <p>
+      <font color="#008000">});</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="styles.topic" ID="ID_1854957515" CREATED="1598722144749" MODIFIED="1598739870374"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      version &#8805; <b><i>ES6</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_1934839155" CREATED="1598732100397" MODIFIED="1598738352920" HGAP_QUANTITY="11.000000089406939 pt" VSHIFT_QUANTITY="137.99999588727962 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>Array.from</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_1808912917" CREATED="1598736953683" MODIFIED="1598739655536"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>const arrayLike = { </b>
+    </p>
+    <p>
+      <b>0: 'Value 0', </b>
+    </p>
+    <p>
+      <b>1: 'Value 1', </b>
+    </p>
+    <p>
+      <b>length: 2 </b>
+    </p>
+    <p>
+      <b>}; </b>
+    </p>
+    <p>
+      <b><font color="#008000">const realArray = Array.from(arrayLike); </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">realArray.forEach(value =&gt; {/* Do something */});</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_1115674813" CREATED="1598732177408" MODIFIED="1598738416291" HGAP_QUANTITY="22.99999973177911 pt" VSHIFT_QUANTITY="-16.49999950826167 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>for...of</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_809959301" CREATED="1598736877862" MODIFIED="1598739669279" HGAP_QUANTITY="58.24999868124728 pt" VSHIFT_QUANTITY="-31.49999906122686 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#008000">var realArray = []; </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">for(const element of arrayLike) { </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">realArray.append(element); </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">}</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_1057030230" CREATED="1598732256396" MODIFIED="1598738399528" HGAP_QUANTITY="19.24999984353784 pt" VSHIFT_QUANTITY="-19.49999941885477 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>Spread operator </i>[...arrayLike]</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_249856593" CREATED="1598741069870" MODIFIED="1598742914491"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      With ES6, you can use spreads to separate individual elements into a comma-separated syntax:
+    </p>
+    <p>
+      <b><font color="#ff0000">let arr = [1, 2, 3, ...[4, 5, 6]]; </font></b>
+    </p>
+    <p>
+      <b><font color="#ff0000">&#160;// [1, 2, 3, 4, 5, 6] </font></b>
+    </p>
+    <p>
+      <b><font color="#ff0000">// in ES &lt; 6, </font></b>
+    </p>
+    <p>
+      <b><font color="#ff0000">// the operations above are equivalent to </font></b>
+    </p>
+    <p>
+      <b><font color="#ff0000">arr = [1, 2, 3]; </font></b>
+    </p>
+    <p>
+      <b><font color="#ff0000">arr.push(4, 5, 6);</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_548388019" CREATED="1598736313406" MODIFIED="1598738424376" HGAP_QUANTITY="25.249999664723884 pt" VSHIFT_QUANTITY="-166.4999950379135 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>Object.keys</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_802890771" CREATED="1598736583111" MODIFIED="1598739681234"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#008000">var realArray = Object </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">.keys(arrayLike) </font></b>
+    </p>
+    <p>
+      <b><font color="#008000">.map((key) =&gt; arrayLike[key]);</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node LOCALIZED_STYLE_REF="styles.topic" ID="ID_21355292" CREATED="1598722166622" MODIFIED="1598739875177"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      version &#8805; <b>ES7</b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_559339123" CREATED="1598736360430" MODIFIED="1598737981889"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><i>Object.values</i></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node LOCALIZED_STYLE_REF="defaultstyle.attributes" ID="ID_546033727" CREATED="1598736398785" MODIFIED="1598739686805" HGAP_QUANTITY="20.74999979883433 pt" VSHIFT_QUANTITY="18.749999441206473 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#008000">var realArray = Object.values(arrayLike);</font></b>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
